c8eec3f22939ab540273f82cb79ce5119498f8f2
vilko <helgi8@mail.ru>
2018-10-10 04:49:59 +0300

    Добавьте файлы проекта.

---
+++ Wanted methods!/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wanted_methods_
+{
+    class Program
+    {
+        static string GetLastHalf(string text)
+        {
+            int length = text.Length;
+            text = text.Substring(length / 2);
+            text = text.Replace(" ", string.Empty);
+            return text;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+    }
+}
